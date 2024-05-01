e7ed1b65a9cb2a4c5f085893ac70433c18c1ad5c
Adnan Tahir <adnantabda@gmail.com>
2024-05-01 20:57:38 +0300

    Add: data holder file with sample

---
+++ src/script/data.js
@@ -0,0 +1,178 @@
+export let projects = [
+    {
+        projectName : "First Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+
+        {
+            name : "Third Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "Second Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project Two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        }, 
+        {
+            name : "Third Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "First Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+
+        {
+            name : "Third Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "Second Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project Two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        }, 
+        {
+            name : "Third Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+]
+
+
+export let habits = [
+    {
+        name : "1st habit exercise",
+        day : ['Monday' , 'Tuesday' , 'Friday'],
+        description : "This Habit serves as testimonial to my strength" , 
+
+    } ,
+    {
+        name : "2nd habit Don't drink alcohol",
+        day : ['Monday' , 'Tuesday' , 'Friday' , 'Thursday' , 'Saturday' , 'Sunday'],
+        description : "This Habit serves as testimonial to my strength" , 
+
+    },    
+    {
+        name : "1st habit exercise",
+        day : ['Monday' , 'Tuesday' , 'Friday'],
+        description : "This Habit serves as testimonial to my strength" , 
+
+    } ,
+    {
+        name : "2nd habit Don't drink alcohol",
+        day : ['Monday' , 'Tuesday' , 'Friday' , 'Thursday' , 'Saturday' , 'Sunday'],
+        description : "This Habit serves as testimonial to my strength" , 
+
+    },  
+    
+]
+
+
+
+export let tasks = [
+    {
+        name : '1st task Taking care of baby', 
+        range : [4 , "hr"], 
+        description : "taking care of a baby for 4 hours after his mother went to super market", 
+        done : false
+    },
+    {
+        name : '2st task Taking care of baby', 
+        range : [4 , "hr"], 
+        description : "taking care of a baby for 4 hours after his mother went to super market", 
+        done : false
+    }, 
+    {
+        name : '1st task Taking care of baby', 
+        range : [4 , "hr"], 
+        description : "taking care of a baby for 4 hours after his mother went to super market", 
+        done : false
+    },
+    
+
+]
