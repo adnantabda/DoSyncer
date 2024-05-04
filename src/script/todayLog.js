e02774597dd45a2925f4acc3ae792fe8c9775ede
Adnan Tahir <adnantabda@gmail.com>
2024-05-04 21:04:36 +0300

    Initialized local storage with project data and implemented functions to retrieve project, habit, and task names.

--- src/script/todayLog.js
+++ src/script/todayLog.js
@@ -1,14 +1,270 @@
-import {projects , habits , tasks } from './data.js'
+// import { projectsJSon } from "./data"
+import { tasksJSon } from "./data"
+import { habitsJSon } from "./data"
 
+
+// const projects = JSON.parse(projectsJSon())
+const tasks = JSON.parse(tasksJSon())
+const habits = JSON.parse(habitsJSon())
+
+
+
+let projectsData = [
+    {
+        projectName : "1 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {  
+            name : "First Todo Of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+
+        {
+            name : "Third Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+        
+        ]
+    },
+    {
+        projectName : "2 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project Two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        }, 
+        {
+            name : "Third Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "3 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+
+        {
+            name : "Third Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "4 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project Two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        }, 
+        {
+            name : "Third Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "5 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+
+        {
+            name : "Third Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+        
+        ]
+    },
+    {
+        projectName : "6 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project Two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        }, 
+        {
+            name : "Third Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "7 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+
+        {
+            name : "Third Todo of Project one",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+    {
+        projectName : "8 Project", 
+        description : "This the project of life that I am working on ", 
+        todo : 
+        [
+        {   
+            name : "First Todo Of Project Two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        },
+        {
+            name : "Second Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if i allah wills", 
+            priority : 2,
+        }, 
+        {
+            name : "Third Todo of Project two",
+            date : "23-14-2024",
+            done : false,
+            description : "this the project that I will work on in the distance future if  allah wills", 
+            priority : 2,
+        }
+
+
+        ]
+    },
+]
+
+
+
+
+
+localStorage.setItem("projects" , JSON.stringify(projectsData))
 
 
 export const allProjectName = ()=>{
-    const projectsNumber = projects.length
-    const projectsList = []
-    for(let i = 0 ; i < projectsNumber ; i++){
-        projectsList.push(projects[i].projectName)
-    }
+    let projectsList = JSON.parse(localStorage.getItem("projects"))
 
+    // console.log(projectsList[0].projectName)
     return projectsList
 
 }
@@ -26,7 +282,6 @@ export const allNameTodoInProject = (allTodoInProject)=>{
         nameList.push(vav[i].name)
     }
     return nameList
-
 
 }
 
